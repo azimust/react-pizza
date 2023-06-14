c52a6b9b84d67f0f72b56a34de1aecc4086b0419
azimust <woody.azimbek@gmail.com>
2023-06-14 11:39:13 +0600

    feat: add types of pizza

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
           </div>
           <h2 class="content__title">Все пиццы</h2>
           <div class="content__items">
-            <PizzaBlock title='Чизбургер-пицца' price='395'/>
-            <PizzaBlock title='Чизбургер-пицца' price='395'/>
-            <PizzaBlock title='Чизбургер-пицца' price='395'/>
-            <PizzaBlock title='Чизбургер-пицца' price='395'/>
+            <PizzaBlock id={1} image='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg' title='Чизбургер-пицца' price={395}/>
+            <PizzaBlock id={2} image='https://dodopizza-a.akamaihd.net/static/Img/Products/9cf62eab14b64d6a8ce0a9f5710f4f76_292x292.webp' title='Песто' price={500}/>
+            <PizzaBlock id={3} image='https://dodopizza-a.akamaihd.net/static/Img/Products/22eead365d6a451eb0e398a6bab07823_292x292.webp' title='Пепперони' price={600}/>
+            <PizzaBlock id={4} image='https://dodopizza-a.akamaihd.net/static/Img/Products/ebbfdf6f8fa74935af77b784a1ff2e1d_292x292.webp' title='Аррива' price={445}/>
           </div>
         </div>
       </div>
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const PizzaBlock = ({image, title, price}) => {
+const PizzaBlock = ({image, title, price, id}) => {
     return (
-        <div class="pizza-block">
-            <img class="pizza-block__image" src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg" alt="Pizza"/>
+        <div class="pizza-block" key={id}>
+            <img class="pizza-block__image" src={image} alt="Pizza"/>
                 <h4 class="pizza-block__title">{title}</h4>
                 <div class="pizza-block__selector">
                     <ul>
@@ -17,7 +17,7 @@ const PizzaBlock = ({image, title, price}) => {
                     </ul>
                 </div>
                 <div class="pizza-block__bottom">
-                    <div class="pizza-block__price">от {price} ₽</div>
+                    <div class="pizza-block__price">от {price} сом</div>
                     <div class="button button--outline button--add">
                         <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="white"></path>
