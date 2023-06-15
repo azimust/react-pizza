6e4b32c51463493da8fd04f3a0a04ef540220ffb
azimust <woody.azimbek@gmail.com>
2023-06-15 20:33:30 +0600

    feat: add pizzas from backend (mockAPI)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
 import './scss/app.scss';
-import pizzas from './data/pizzas.json'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('https://648b18e717f1536d65ea596a.mockapi.io/items')
+    .then(res => res.json())
+    .then(arr => setPizzas(arr))
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
